47fa10a0dd07c239f59455fc99efbccee6b7ebbb
Jay Gillenwater <jhgille2@ncsu.edu>
2022-01-27 21:05:53 -0500

    added fit stepwise regression models for the average n/s ratio trait

--- tests/parallel mapping tests.R
+++ tests/parallel mapping tests.R
@@ -23,7 +23,45 @@ stepwise_n_s_imp <- stepwiseqtl(LinkageMap_simgeno,
 
 drawnums <- c(100, 250, 500, 750, 1000)
 
-future::plan(remote, workers = paste0("node", str_pad(0:5, 3, pad = "0")), verbose = TRUE)
+future::plan(multisession, workers = 5)
 
-filled_genos <- future_map(drawnums, function(x) sim.geno(LinkageMap, n.draws = x, map.function = "kosambi"))
-all_stepwise <- future_map(drawnums, function(x) stepwiseqtl(x, pheno.col = 2, max.qtl = 20, method = "imp", penalties = scantwo_pens))
+filled_genos <- future_map(drawnums, function(x) sim.geno(LinkageMap, n.draws = x, map.function = "kosambi"), .options = furrr_options(seed = TRUE))
+all_stepwise <- future_map(filled_genos, function(x) stepwiseqtl(x, pheno.col = 2, max.qtl = 20, method = "imp", penalties = scantwo_pens), .options = furrr_options(seed = TRUE))
+
+
+test <- fitqtl(filled_genos[[1]], pheno.col = 2, qtl = all_stepwise[[1]])
+test2 <- dropfromqtl(all_stepwise[[1]], which(test$result.drop[, 7] > 0.05))
+
+# A function to drop non-significant (pvalue(F) > 0.05) from the fit model and 
+# return the new model minus these terms
+drop_non_significant_qtl <- function(Cross, qtl_obj)
+{
+  # First, fit the full model
+  full_model    <- fitqtl(cross = Cross, pheno.col = 2, qtl_obj)
+  
+  # Drop qtl with a f value less than 0.05
+  reduced_model <- dropfromqtl(qtl_obj, which(full_model$result.drop[, 7] > 0.05))
+  
+  final_model <- fitqtl(Cross, pheno.col = 2, reduced_model)
+  
+  return(list(qtl = reduced_model, fit = final_model))
+}
+
+# All the reduced models
+all_reduced <- map2(filled_genos, all_stepwise, drop_non_significant_qtl)
+
+hk_stepwise <- stepwiseqtl(calc.genoprob(LinkageMap), 
+                           pheno.col = 2, 
+                           max.qtl = 20,
+                           method = "hk", 
+                           keeplodprofile = TRUE, 
+                           penalties = calc.penalties(scantwo_perms))
+
+drop_non_significant_qtl(calc.genoprob(LinkageMap), hk_stepwise)$result.drop
+
+test_nitrogen_hk_stepwise <- stepwiseqtl(calc.genoprob(LinkageMap), 
+                                         pheno.col = 5, 
+                                         max.qtl = 20, 
+                                         method = "hk", 
+                                         keeplodprofile = TRUE, 
+                                         penalties = calc.penalties(scantwo_perms))
